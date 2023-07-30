db3e43e21525379da917aa2426388a3861574986
João Junior <batistasousajoao@gmail.com>
2023-07-30 10:35:57 -0300

    Adicionar arquivos de projeto.

---
+++ retangulo1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Formats.Asn1;
+using System.Globalization;
+
+
+namespace retangulo1
+{
+    public class _retangulo1
+    {
+        static void Main(string[] args)
+        {
+            Retangulo valores = new Retangulo();
+
+            Console.WriteLine("Entre com a largura e altura: ");
+            valores.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            valores.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("AREA = " + valores.CalculoArea().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERÍMETRO = " + valores.CalculoPerimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + valores.CalculoDiagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
---
+++ retangulo1/Retangulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace retangulo1
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double CalculoArea()
+        {
+            return Largura * Altura;
+        }
+
+        public double CalculoPerimetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+
+        public double CalculoDiagonal()
+        {
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+
+
+
+    }
+
+
+
+
+}
